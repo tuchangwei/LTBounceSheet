d5c41f8652df664807ca6de6eb951aaf8967f6a3
vale <changweitu@gmail.com>
2015-05-20 18:42:14 +0800

    convert OC to Swift.

--- LTBounceSheet/ViewController.swift
+++ LTBounceSheet/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
+let color = UIColor(red: 0/255.0, green: 175/255.0, blue: 240/255.0, alpha: 1)
 class ViewController: UIViewController {
 
+    var sheet: LTBounceSheet!
+    var shown: Bool!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        self.sheet = LTBounceSheet(height: 250, bgColor: color)
+        
+        let option1 = produceButtonWithTitle("take photo")
+        option1.frame=CGRectMake(15, 30, 290, 46);
+        self.sheet.addView(option1)
+        
+        let option2 = produceButtonWithTitle("choose existing photo")
+        option2.frame=CGRectMake(15, 90, 290, 46);
+        self.sheet.addView(option2)
+        
+        
+        let cancel = produceButtonWithTitle("cancel")
+        cancel.frame=CGRectMake(15, 170, 290, 46);
+        self.sheet.addView(cancel)
+        
+        
+        
     }
-
+    
+    func produceButtonWithTitle(title: String) -> UIButton {
+        
+        var button = UIButton.buttonWithType(.Custom) as! UIButton
+        button.backgroundColor = UIColor.whiteColor()
+        button.layer.cornerRadius=23
+        button.titleLabel!.textAlignment = .Center;
+        button.titleLabel!.font = UIFont(name: "Helvetica Neue", size: 16)
+        
+        button.setTitle(title, forState: .Normal)
+        button.setTitleColor(color, forState: .Normal)
+        return button;
+    }
+    @IBAction func toggle(sender: AnyObject) {
+        
+        self.sheet.toggle() 
+    }
 
 }
 
